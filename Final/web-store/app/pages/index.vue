6214e6f846ca02c678fe7586d00df35c0dff30fa
Michael Omonedo <rukkyomonedo@gmail.com>
2026-05-24 19:10:18 +0100

    feat: integrate api endpoints to display store items on frontend

---
+++ Final/web-store/app/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Rukky's Web Store!</h1>
+    <p>Explore our wide range of products and find the best deals.</p>
+    <div class="product-list">
+      <ProductCard />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  padding: 20px;
+  background-color: #f5f5f5;
+}
+.product-list {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  gap: 20px;
+  margin-top: 20px;
+  background-color: #fff;
+}
+</style>
---
+++ Initial/web-store/app/components/productCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const { data } = await useFetch('https://fakestoreapi.com/products') 
+console.log(data)
+
+</script>
+
+<template>
+    <div v-for="product in data" :key="product.id" class="product-card">
+        <img :src="product.image" class="product-image" :alt="product.title" />
+        <p class="product-title">{{ product.title }} - ${{  product.price  }}</p>
+        <Button />
+    </div>
+</template>
+
+<style scoped>
+
+.product-card {
+  width: 40vw;
+  padding: 10px 10px 10px 10px;
+  border: 2px solid #ccc;
+  border-radius: 5px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.product-image {
+  width: 50%;
+}
+
+.product-title {
+  font-size: 14px;
+}
+</style>
